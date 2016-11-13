cf6b250b33905f02e600042c16ffaac1c12e011e
Eric Dejonckheere <eric@aya.io>
2016-11-13 13:56:44 +0100

    hashmap is read from its own fn

--- src/decode.rs
+++ src/decode.rs
@@ -6,49 +6,8 @@ pub fn translated(words: Vec<String>, yell: bool) -> String {
 }
 
 fn translate(words: Vec<String>) -> Vec<String> {
-    let dic: HashMap<&str, &str> = [("alpha", "a"),
-                                    ("bravo", "b"),
-                                    ("charlie", "c"),
-                                    ("delta", "d"),
-                                    ("echo", "e"),
-                                    ("foxtrot", "f"),
-                                    ("golf", "g"),
-                                    ("hotel", "h"),
-                                    ("india", "i"),
-                                    ("juliet", "j"),
-                                    ("kilo", "k"),
-                                    ("lima", "l"),
-                                    ("mike", "m"),
-                                    ("november", "n"),
-                                    ("oscar", "o"),
-                                    ("papa", "p"),
-                                    ("quebec", "q"),
-                                    ("romeo", "r"),
-                                    ("sierra", "s"),
-                                    ("tango", "t"),
-                                    ("uniform", "u"),
-                                    ("victor", "v"),
-                                    ("whiskey", "w"),
-                                    ("x-ray", "x"),
-                                    ("yankee", "y"),
-                                    ("zulu", "z"),
-                                    ("zero", "0"),
-                                    ("one", "1"),
-                                    ("two", "2"),
-                                    ("three", "3"),
-                                    ("four", "4"),
-                                    ("five", "5"),
-                                    ("six", "6"),
-                                    ("seven", "7"),
-                                    ("eight", "8"),
-                                    ("niner", "9"),
-                                    ("stop", ".")]
-        .iter()
-        .cloned()
-        .collect();
-
+    let dic = get_map();
     let mut bucket: Vec<String> = Vec::new();
-
     for word in words {
         let low = word.to_lowercase();
         let lref: &str = low.as_ref();
@@ -58,6 +17,48 @@ fn translate(words: Vec<String>) -> Vec<String> {
         };
         bucket.push(letter);
     }
-
     bucket
+}
+
+fn get_map<'a>() -> HashMap<&'a str, &'a str> {
+    [("alpha", "a"),
+     ("bravo", "b"),
+     ("charlie", "c"),
+     ("delta", "d"),
+     ("echo", "e"),
+     ("foxtrot", "f"),
+     ("golf", "g"),
+     ("hotel", "h"),
+     ("india", "i"),
+     ("juliet", "j"),
+     ("kilo", "k"),
+     ("lima", "l"),
+     ("mike", "m"),
+     ("november", "n"),
+     ("oscar", "o"),
+     ("papa", "p"),
+     ("quebec", "q"),
+     ("romeo", "r"),
+     ("sierra", "s"),
+     ("tango", "t"),
+     ("uniform", "u"),
+     ("victor", "v"),
+     ("whiskey", "w"),
+     ("x-ray", "x"),
+     ("yankee", "y"),
+     ("zulu", "z"),
+     ("zero", "0"),
+     ("one", "1"),
+     ("two", "2"),
+     ("three", "3"),
+     ("four", "4"),
+     ("five", "5"),
+     ("six", "6"),
+     ("seven", "7"),
+     ("eight", "8"),
+     ("niner", "9"),
+     ("stop", ".")]
+        .iter()
+        .cloned()
+        .collect()
 }
--- src/encode.rs
+++ src/encode.rs
@@ -6,52 +6,11 @@ pub fn translated(words: Vec<String>, yell: bool) -> String {
 }
 
 fn translate(words: Vec<String>) -> Vec<String> {
-    let dic: HashMap<&str, &str> = [("a", "alpha"),
-                                    ("b", "bravo"),
-                                    ("c", "charlie"),
-                                    ("d", "delta"),
-                                    ("e", "echo"),
-                                    ("f", "foxtrot"),
-                                    ("g", "golf"),
-                                    ("h", "hotel"),
-                                    ("i", "india"),
-                                    ("j", "juliet"),
-                                    ("k", "kilo"),
-                                    ("l", "lima"),
-                                    ("m", "mike"),
-                                    ("n", "november"),
-                                    ("o", "oscar"),
-                                    ("p", "papa"),
-                                    ("q", "quebec"),
-                                    ("r", "romeo"),
-                                    ("s", "sierra"),
-                                    ("t", "tango"),
-                                    ("u", "uniform"),
-                                    ("v", "victor"),
-                                    ("w", "whiskey"),
-                                    ("x", "x-ray"),
-                                    ("y", "yankee"),
-                                    ("z", "zulu"),
-                                    ("0", "zero"),
-                                    ("1", "one"),
-                                    ("2", "two"),
-                                    ("3", "three"),
-                                    ("4", "four"),
-                                    ("5", "five"),
-                                    ("6", "six"),
-                                    ("7", "seven"),
-                                    ("8", "eight"),
-                                    ("9", "niner"),
-                                    (".", "stop")]
-        .iter()
-        .cloned()
-        .collect();
-
+    let dic = get_map();
     let mut bucket: Vec<String> = Vec::new();
     let mut count = 0;
     let len = words.len();
-
-    for word in &words {
+    for word in words {
         count += 1;
         let low = word.to_lowercase();
         let ref chs: Vec<&str> = low.split("").collect();
@@ -72,6 +31,48 @@ fn translate(words: Vec<String>) -> Vec<String> {
             bucket.push("- ".to_string());
         }
     }
-
     bucket
+}
+
+fn get_map<'a>() -> HashMap<&'a str, &'a str> {
+    [("a", "alpha"),
+     ("b", "bravo"),
+     ("c", "charlie"),
+     ("d", "delta"),
+     ("e", "echo"),
+     ("f", "foxtrot"),
+     ("g", "golf"),
+     ("h", "hotel"),
+     ("i", "india"),
+     ("j", "juliet"),
+     ("k", "kilo"),
+     ("l", "lima"),
+     ("m", "mike"),
+     ("n", "november"),
+     ("o", "oscar"),
+     ("p", "papa"),
+     ("q", "quebec"),
+     ("r", "romeo"),
+     ("s", "sierra"),
+     ("t", "tango"),
+     ("u", "uniform"),
+     ("v", "victor"),
+     ("w", "whiskey"),
+     ("x", "x-ray"),
+     ("y", "yankee"),
+     ("z", "zulu"),
+     ("0", "zero"),
+     ("1", "one"),
+     ("2", "two"),
+     ("3", "three"),
+     ("4", "four"),
+     ("5", "five"),
+     ("6", "six"),
+     ("7", "seven"),
+     ("8", "eight"),
+     ("9", "niner"),
+     (".", "stop")]
+        .iter()
+        .cloned()
+        .collect()
 }
